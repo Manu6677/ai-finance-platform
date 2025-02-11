11799c83177962e13928eddb2844b267ff4effae
manu <manu.panjoria666@gmail.com>
2025-02-12 00:10:58 +0530

    added/accountPage-render-category-color/amount/recurring-date/applied-shadcn-menu

--- app/(main)/account/[id]/_components/transaction-table.jsx
+++ app/(main)/account/[id]/_components/transaction-table.jsx
@@ -1,5 +1,15 @@
 "use client";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import {
   Table,
   TableBody,
@@ -9,14 +19,27 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { categoryColors } from "@/data/categories";
 import { format } from "date-fns";
-import React from "react";
+import { Clock, MoreHorizontal, RefreshCcw } from "lucide-react";
+import { useRouter } from "next/router";
 
 const TransactionTable = ({ transaction }) => {
-
   const filteredAndSortedTransaction = transaction;
-  console.log('filteredAndSortedTransaction' ,filteredAndSortedTransaction);
 
+  const recuringIntervals = {
+    MONTHLY: "Monthly",
+    YEARLY: "Yearly",
+    DAILY: "Daily",
+  };
+
+  // const router = useRouter();
   const handleSort = () => {};
 
   return (
@@ -35,39 +58,115 @@ const TransactionTable = ({ transaction }) => {
                 className="cursor-pointer"
                 onClick={() => handleSort("date")}
               >
-               <div className="flex items-center border">Date</div>
+                <div className="flex items-center">Date</div>
               </TableHead>
               <TableHead>Discription</TableHead>
               <TableHead
                 className="cursor-pointer"
                 onClick={() => handleSort("category")}
               >
-               <div className="flex items-center border">Category</div>
+                <div className="flex items-center">Category</div>
               </TableHead>
               <TableHead
                 className="cursor-pointer"
                 onClick={() => handleSort("amount")}
               >
-                <div className="flex items-center border justify-end">Amount</div>
+                <div className="flex items-center justify-end">Amount</div>
               </TableHead>
               <TableHead className="">Recurring</TableHead>
               <TableHead className="w-[50px]" />
             </TableRow>
           </TableHeader>
           <TableBody>
-            { filteredAndSortedTransaction?.length === 0 ? <TableRow>
-              <TableCell colSpan={7} className='text-center text-muted-foreground'>No Transaction Found</TableCell>
-            </TableRow>:(
-              filteredAndSortedTransaction?.map((transaction)=>(
+            {filteredAndSortedTransaction?.length === 0 ? (
+              <TableRow>
+                <TableCell
+                  colSpan={7}
+                  className="text-center text-muted-foreground"
+                >
+                  No Transaction Found
+                </TableCell>
+              </TableRow>
+            ) : (
+              filteredAndSortedTransaction?.map((transaction) => (
                 <TableRow key={transaction.id}>
                   <TableCell className="font-medium">
                     <Checkbox />
                   </TableCell>
-                  <TableCell>{ format(new Date(transaction.date), 'pp') }</TableCell>
+                  <TableCell>
+                    {format(new Date(transaction.date), "PP")}
+                  </TableCell>
                   <TableCell>{transaction?.description}</TableCell>
-                  <TableCell className="">{transaction?.category}</TableCell>
-                  <TableCell className="text-right">{transaction.amount}</TableCell>
-                  <TableCell className="">{ transaction.recurring}</TableCell>
+                  <TableCell className="capitalize">
+                    <span
+                      style={{
+                        background: categoryColors[transaction?.category],
+                      }}
+                      className="px-2 rounded-md text-white text-sm py-1"
+                    >
+                      {transaction?.category}
+                    </span>
+                  </TableCell>
+                  <TableCell
+                    className="text-right font-medium"
+                    style={{
+                      color: transaction.type === "INCOME" ? "red" : "green",
+                    }}
+                  >
+                    {transaction.type === "INCOME" ? "+" : "-"}$
+                    {transaction.amount.toFixed(2)}
+                  </TableCell>
+                  <TableCell className="">
+                    {transaction.isRecurring ? (
+                      <TooltipProvider>
+                        <Tooltip>
+                          <TooltipTrigger>
+                            <Badge
+                              variant="outline"
+                              className="gap-1 bg-purple-100 text-purple-700 hover:bg-purple-200"
+                            >
+                              <RefreshCcw className="h-3 w-3" />{" "}
+                              {recuringIntervals[transaction.recurringInterval]}
+                            </Badge>
+                          </TooltipTrigger>
+                          <TooltipContent>
+                            <div className="text-sm">
+                              <div className="font-medium">Next Date</div>
+                              <div>
+                                {format(
+                                  new Date(transaction.nextRecurringDate),
+                                  "PP"
+                                )}
+                              </div>
+                            </div>
+                          </TooltipContent>
+                        </Tooltip>
+                      </TooltipProvider>
+                    ) : (
+                      <Badge variant="outline" className="gap-1">
+                        <Clock className="h-3 w-3" /> One-time
+                      </Badge>
+                    )}
+                  </TableCell>
+                  <TableCell>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="h-8 w-8 p-0">
+                          <MoreHorizontal className="h-4 w-4" />
+                        </Button>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent>
+                        <DropdownMenuItem
+                        // onClick={router.push(
+                        //   `/transaction/create?edit${transaction.id}`
+                        // )}
+                        >
+                          Edit
+                        </DropdownMenuItem>
+                        <DropdownMenuItem>Delete</DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </TableCell>
                 </TableRow>
               ))
             )}
--- components/create-account-drawer.jsx
+++ components/create-account-drawer.jsx
@@ -1,12 +1,25 @@
-"use client"
+"use client";
 import React, { useEffect } from "react";
-import { Drawer, DrawerClose, DrawerContent, DrawerHeader, DrawerTitle, DrawerTrigger} from "@/components/ui/drawer";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod"
+import { zodResolver } from "@hookform/resolvers/zod";
 import { accountSchema } from "@/app/lib/schema";
 import { Input } from "./ui/input";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
 import { Switch } from "./ui/switch";
 import { Button } from "./ui/button";
 import useFetch from "@/hooks/fetch";
@@ -15,45 +28,54 @@ import { Loader2 } from "lucide-react";
 import { toast } from "sonner";
 
 const CreareAccountDrawer = ({ children }) => {
-    
-    const {data: newAccount, error, fn: createAccountFn, loading: createAccountLoading} = useFetch(createAccount);
+  const {
+    data: newAccount,
+    error,
+    fn: createAccountFn,
+    loading: createAccountLoading,
+  } = useFetch(createAccount);
 
-    useEffect(()=>{
+  useEffect(() => {
+    console.log("useEffect", newAccount);
+    if (newAccount && !createAccountLoading) {
+      toast.success("Account Created Successfully");
+      reset();
+      setOpen(false);
+    }
+  }, [createAccountLoading, newAccount]);
 
-      console.log('useEffect', newAccount);
-      if(newAccount && !createAccountLoading){
-        toast.success('Account Created Successfully');
-        reset();
-        setOpen(false);
-      }
+  useEffect(() => {
+    if (error) {
+      toast.error(error.message || "failed to create a new account");
+    }
+  }, [error]);
 
-    },[createAccountLoading, newAccount])
+  const [open, setOpen] = useState(false);
 
-    useEffect(()=>{
-      if(error){
-        toast.error(error.message || "failed to create a new account")
-      }
-    },[error])
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setValue,
+    watch,
+    reset,
+  } = useForm({
+    resolver: zodResolver(accountSchema),
+    defaultValues: {
+      name: "",
+      type: "CURRENT",
+      balance: "",
+      isDefault: false,
+    },
+  });
 
-    const [open, setOpen] = useState(false);
+  const onSubmit = async (data) => {
+    console.log("Form Data:", data);
+    createAccountFn(data);
+  };
 
-    const {register, handleSubmit, formState:{errors}, setValue, watch, reset} = useForm({
-      resolver: zodResolver(accountSchema),
-      defaultValues: {
-        name: "",
-        type: "CURRENT",
-        balance: "",
-        isDefault: false
-      }
-    })
-
-    const onSubmit = async (data)=>{
-      console.log('Form Data:', data);
-      createAccountFn(data)
-    } 
-
-    // console.log(open);
-/*
+  // console.log(open);
+  /*
     What Happens If You Remove prop={open} onOpenChange={setOpen}?
     The Drawer component would then use its internal state for managing the open/close behavior instead of being externally controlled.
     This means you won't have direct control of the open state from your component (e.g., via useState).
@@ -61,9 +83,9 @@ const CreareAccountDrawer = ({ children }) => {
     
     Without prop={open}, the drawer would still open and close, but the external open state in your component would not reflect or control this behavior.
     Without onOpenChange={setOpen}, closing the drawer via interactions (like clicking outside) wouldn't update the external open state.
-*/    
+*/
 
-/*
+  /*
      Quest) Why Use prop={open} onOpenChange={setOpen}?
      ==> Using these props makes the Drawer controlled, meaning its state is fully managed by the useState in your component. This is beneficial because:
 
@@ -72,13 +94,13 @@ const CreareAccountDrawer = ({ children }) => {
      3) Sync with State: The drawer's visibility is always in sync with your application's state.
 */
 
-/*
+  /*
    Here prop={open} is being passed to the drawer component, supposed it accept here as a prop.
     -> prop={open} binds the state of the drawer (which is true or false) to the component.
     --> The Drawer accepts this prop to control its visibility, ensuring it opens when open is true and closes when open is false.
 */
 
-/* 
+  /* 
   1) Example: Without asChild
 
    <DrawerTrigger>
@@ -111,81 +133,131 @@ const CreareAccountDrawer = ({ children }) => {
 */
   return (
     <Drawer prop={open} onOpenChange={setOpen}>
-      <DrawerTrigger asChild>{ children }</DrawerTrigger>
+      <DrawerTrigger asChild>{children}</DrawerTrigger>
       <DrawerContent>
         <DrawerHeader>
           <DrawerTitle>Create New Account</DrawerTitle>
         </DrawerHeader>
 
         {/* Account name */}
-          <div className="px-4 pb-4">
-            <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
-              <div className="space-y-2">
-                  <label htmlFor="name" className="text-sm font-medium"> Account Name </label>
-                  {/* {...register('name')} what connection it is doing here in the input field */}
-                  <Input id='name' placeholder="eg. Main Checking"  {...register('name')} />
-                  {errors.name && <p className="text-sm text-red-500">{errors.name.message}</p>}                          
-              </div>
-   
-          {/* Account type */}
+        <div className="px-4 pb-4">
+          <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
             <div className="space-y-2">
-                  <label htmlFor="type" className="text-sm font-medium"> Account Type </label>
-                {/* defaultValue={watch('type') is used to retrive the current value of the type */}
-                  <Select onValueChange={(value)=> setValue("type", value)}
-                    defaultValue={watch('type')}
-                    >
-                   <SelectTrigger id='type'>
-                    <SelectValue placeholder="Select Account Type" />
-                  </SelectTrigger>
-                  <SelectContent>
-                     <SelectItem value="CURRENT">Current</SelectItem>
-                     <SelectItem value="SAVING">Saving</SelectItem>
-                  </SelectContent>
-                 </Select>
+              <label htmlFor="name" className="text-sm font-medium">
+                Account Name
+              </label>
+              {/* {...register('name')} what connection it is doing here in the input field */}
+              <Input
+                id="name"
+                placeholder="eg. Main Checking"
+                {...register("name")}
+              />
+              {errors.name && (
+                <p className="text-sm text-red-500">{errors.name.message}</p>
+              )}
+            </div>
 
-                  {errors.type && <p className="text-sm text-red-500">{errors.type.message}</p>}                          
-             </div>
+            {/* Account type */}
+            <div className="space-y-2">
+              <label htmlFor="type" className="text-sm font-medium">
+                {" "}
+                Account Type{" "}
+              </label>
+              {/* defaultValue={watch('type') is used to retrive the current value of the type */}
+              <Select
+                onValueChange={(value) => setValue("type", value)}
+                defaultValue={watch("type")}
+              >
+                <SelectTrigger id="type">
+                  <SelectValue placeholder="Select Account Type" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem value="CURRENT">Current</SelectItem>
+                  <SelectItem value="SAVING">Saving</SelectItem>
+                </SelectContent>
+              </Select>
 
-               {/* balance  */}          
-                <div className="space-y-2">
-                  <label htmlFor="balance" className="text-sm font-medium">Initial Balance </label>
-                   {/* what is step  ==> step="0.01", means to deal with two input places */}
-                  <Input id='balance' type='number' step='0.01' placeholder="0.00"  {...register('balance')}/>
-                  {errors.balance && <p className="text-sm text-red-500">{errors.balance.message}</p>}                          
-                </div>
+              {errors.type && (
+                <p className="text-sm text-red-500">{errors.type.message}</p>
+              )}
+            </div>
 
-        {/* switch  */}
-          <div className="flex items-center justify-between rounded-lg border p-3">
+            {/* balance  */}
+            <div className="space-y-2">
+              <label htmlFor="balance" className="text-sm font-medium">
+                Initial Balance{" "}
+              </label>
+              {/* what is step  ==> step="0.01", means to deal with two input places */}
+              <Input
+                id="balance"
+                type="number"
+                step="0.01"
+                placeholder="0.00"
+                {...register("balance")}
+              />
+              {errors.balance && (
+                <p className="text-sm text-red-500">{errors.balance.message}</p>
+              )}
+            </div>
+
+            {/* switch  */}
+            <div className="flex items-center justify-between rounded-lg border p-3">
               <div className="space-y-0.5">
-                <label htmlFor="isDefault" className="text-sm font-medium cursor-pointer">Set As Default</label>
-                <p className="text-sm text-muted-foreground">This account will be selected by default for transaction</p>
+                <label
+                  htmlFor="isDefault"
+                  className="text-sm font-medium cursor-pointer"
+                >
+                  Set As Default
+                </label>
+                <p className="text-sm text-muted-foreground">
+                  This account will be selected by default for transaction
+                </p>
               </div>
-                <Switch id='isDefault' onCheckedChange={(checked)=> setValue('isDefault', checked)}
+              <Switch
+                id="isDefault"
+                onCheckedChange={(checked) => setValue("isDefault", checked)}
                 //  checked={watch('isDefault')} is used to make the switch in sycn with the form state
-                   checked={watch('isDefault')}
-                />
-              {errors.isDefault && <p className="text-sm text-red-500">{errors.isDefault.message}</p>}                          
-          </div>  
+                checked={watch("isDefault")}
+              />
+              {errors.isDefault && (
+                <p className="text-sm text-red-500">
+                  {errors.isDefault.message}
+                </p>
+              )}
+            </div>
 
-      {/* button for cenceling and creating an account */}
-      <div className="p-3 grid-cols-2 space-x-3 flex items-center justify-center">
+            {/* button for cenceling and creating an account */}
+            <div className="p-3 grid-cols-2 space-x-3 flex items-center justify-center">
+              <DrawerClose asChild>
+                <Button type="button" variant="outline" className="flex-1">
+                  Cancel
+                </Button>
+              </DrawerClose>
 
-        <DrawerClose asChild>
-          <Button type='button' variant="outline" className='flex-1'>Cancel</Button>
-        </DrawerClose>
-
-        <Button className='flex-1' type='submit' disabled={createAccountLoading}>{createAccountLoading? <><Loader2 className="animate-spin h-4 w-4 mr-2"/>Creating...</>: 'create Account'}</Button>
-        {/* <Button className='flex-1' type='submit'>Create Account</Button> */}
-      </div>
-    </form>
- </div>
+              <Button
+                className="flex-1"
+                type="submit"
+                disabled={createAccountLoading}
+              >
+                {createAccountLoading ? (
+                  <>
+                    <Loader2 className="animate-spin h-4 w-4 mr-2" />
+                    Creating...
+                  </>
+                ) : (
+                  "create Account"
+                )}
+              </Button>
+              {/* <Button className='flex-1' type='submit'>Create Account</Button> */}
+            </div>
+          </form>
+        </div>
       </DrawerContent>
     </Drawer>
   );
 };
 
 export default CreareAccountDrawer;
-
 
 /*
 Key Points:
